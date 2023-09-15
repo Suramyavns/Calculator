c96c7814c26278b18eb94a654aa7f1c3f2f03577
Suramyavns <96076894+Suramyavns@users.noreply.github.com>
2023-09-15 19:35:58 +0530

    added more features

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 function operate(e){
     var textbox = document.getElementById('number-display')
     if(e.value==='clear'){
-        textbox.innerHTML = ''
+        textbox.value = ''
         operation = []
     }
     else if(e.value==='='){
         operation = [eval(operation.join('')).toFixed(2)]
-        textbox.innerHTML = operation.join('')
+        textbox.value = operation.join('')
     }
     else if(e.value==='back'){
         operation.pop();
-        textbox.innerHTML = operation.join('')
+        textbox.value = operation.join('')
     }
     else{
         operation.push(e.value)
-        textbox.innerHTML = operation.join('');
+        textbox.value = operation.join('');
     }
 }
 var operation = []
+var input = document.getElementById('number-display');
+input.addEventListener("keydown",(event) =>{
+    if (event.key === "Enter") {
+        operation.push(input.value.split())
+        document.getElementById('equals').click()
+        operation = []
+    }
+    else if(event.keyCode === 46){
+        document.getElementById('clear').click()
+    }
+})
